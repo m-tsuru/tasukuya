53976d4c49e4075ef22120ebcd295fbe443206f2
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2025-09-25 02:03:26 +0900

    feat: 🎸 Create Bot

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import asyncio
 import signal
+import sys
+
+import discord
+
+from discord.ext import commands
+
+# 定数
+COMMAND_PREFIX_DEFAULT = "/"
+DISCORD_TOKEN_UNSETTED_DEFAULT = "UNSET"  # noqa: S105
+
+# トークン
+DISCORD_TOKEN = "UNSET"  # noqa: S105
+
+intents = discord.Intents.default()
+intents.message_content = True
 
 
 async def _main() -> None:
+    bot = commands.Bot(command_prefix=COMMAND_PREFIX_DEFAULT, intents=intents)
+
     stop_event = asyncio.Event()
 
     async def stop() -> None:
+        await bot.close()
         stop_event.set()
 
     loop = asyncio.get_running_loop()
     for sig in (signal.SIGINT, signal.SIGTERM):
         loop.add_signal_handler(sig, lambda: asyncio.create_task(stop()))
+
+    if DISCORD_TOKEN == DISCORD_TOKEN_UNSETTED_DEFAULT:
+        sys.exit(1)
+
+    await bot.start()
     await stop_event.wait()
 
 
-async def main() -> None:
+def main() -> None:
     asyncio.run(_main())
 
 
