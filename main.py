155b4925c340d58986cd259220ede06bba464645
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2025-09-25 01:55:54 +0900

    feat: 🎸 [WIP] SIGINT / SIGTERM

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-def main() -> None:
-    pass
+import asyncio
+import signal
+
+
+async def _main() -> None:
+    stop_event = asyncio.Event()
+
+    async def stop() -> None:
+        stop_event.set()
+
+    loop = asyncio.get_running_loop()
+    for sig in (signal.SIGINT, signal.SIGTERM):
+        loop.add_signal_handler(sig, lambda: asyncio.create_task(stop()))
+    await stop_event.wait()
+
+
+async def main() -> None:
+    asyncio.run(_main())
+
 
 if __name__ == "__main__":
     main()
